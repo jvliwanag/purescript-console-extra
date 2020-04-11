3e4cb21090167affac7fc80f91a225269ab41dce
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-04-11 20:00:19 +0800

    Add test sample calls

--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,12 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console.Extra (info, log, table)
+import Effect.Console.Extra (error2, info', log, table)
+import Effect.Exception as Exception
 
 main :: Effect Unit
 main = do
   log "🍝"
-  info ["🍍", "✒️b"]
+  info' ["🍍", "✒️"]
   table [1, 2, 3]
+  error2 "Uh oh" (Exception.error "kaboom")
